3bda7750aecc4e10ba88fb183e36b0a1368d3c3c
chcolleoni <chcolleoni@gmail.com>
2021-03-19 12:56:20 -0300

    add auth

--- src/main/java/com/bluet/bring/getnewsrest/SecurityConfiguration.java
+++ src/main/java/com/bluet/bring/getnewsrest/SecurityConfiguration.java
@@ -53,7 +53,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 
 		http
-		//.cors().and()
+		.cors().and()
 		//.csrf().disable()		
 		.authorizeRequests()
 		.antMatchers(HttpMethod.POST, "/auth/sign-in").permitAll()
