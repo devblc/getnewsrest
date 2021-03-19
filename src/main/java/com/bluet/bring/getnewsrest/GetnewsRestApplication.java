36a90fe4b4a51197fdf176cf060759732d26f876
chcolleoni <chcolleoni@gmail.com>
2021-03-19 13:58:36 -0300

    add auth

--- src/main/java/com/bluet/bring/getnewsrest/GetnewsRestApplication.java
+++ src/main/java/com/bluet/bring/getnewsrest/GetnewsRestApplication.java
@@ -2,15 +2,14 @@ package com.bluet.bring.getnewsrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import com.bluet.bring.getnewsrest.auth.repo.UserRepository;
 
-
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.bluet.bring.getnewsrest")
 @EnableMongoRepositories(basePackages = {"com.bluet.bring.getnewsrest.repo", 
 		"com.bluet.bring.getnewsrest.auth.repo", "com.bluet.bring.getnewsrest.auth.service"})
-//@EnableRepositories(basePackageClasses = UserRepository.class)
 public class GetnewsRestApplication {
 
 	public static void main(String[] args) {
