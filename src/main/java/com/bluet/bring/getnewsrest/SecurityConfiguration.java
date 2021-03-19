69e4f136953a8757cf089fb2ca397832156c5aa6
chcolleoni <chcolleoni@gmail.com>
2021-03-19 13:05:39 -0300

    add auth

--- src/main/java/com/bluet/bring/getnewsrest/SecurityConfiguration.java
+++ src/main/java/com/bluet/bring/getnewsrest/SecurityConfiguration.java
@@ -1,16 +1,10 @@
 package com.bluet.bring.getnewsrest;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,11 +13,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
@@ -54,7 +46,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
 		http
 		.cors().and()
-		//.csrf().disable()		
 		.authorizeRequests()
 		.antMatchers(HttpMethod.POST, "/auth/sign-in").permitAll()
 		.antMatchers(HttpMethod.POST, "/auth/sign-up").permitAll()
@@ -65,6 +56,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 		
 		http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 		
+		//.csrf().disable()		
 		//.antMatchers("/admin").hasRole("ADMIN")
         //.antMatchers("/user").hasAnyRole("ADMIN", "USER")
 		//.and().formLogin()
@@ -94,30 +86,26 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	
 	
     // To enable CORS
-	@Configuration
-	public class CustomCorsConfiguration {
-
-		@Bean
-		public FilterRegistrationBean<CorsFilter> corsFilter() {
-			UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-			CorsConfiguration config = new CorsConfiguration();
-			config.setAllowCredentials(true);
-			config.addAllowedOrigin("http://localhost:8080");
-			config.addAllowedHeader("*");
-			config.addAllowedMethod("*");
-			source.registerCorsConfiguration("/**", config);
-			FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(new CorsFilter(source));
-			bean.setOrder(0);
-			return bean;
-		}
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("http://localhost:8080");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(new CorsFilter(source));
+		bean.setOrder(0);
+		return bean;
 	}
 	
+
 	/*
     // To enable CORS
     @Bean
-    public CorsFilter corsConfigurationFilter() {
+    public CorsFilter corsFilter() {
     	
-        
     	final CorsConfiguration configuration = new CorsConfiguration();
     	//configuration.setAllowedOrigins(List.of("http://localhost:3000") ); // www - obligatory
     	List<String> allowList = new ArrayList<String>();
