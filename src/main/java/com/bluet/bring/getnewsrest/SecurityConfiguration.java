2d5cf27bce5056953b1d993b3e1c6cf71d07cd6e
chcolleoni <chcolleoni@gmail.com>
2021-03-19 13:21:25 -0300

    add auth

--- src/main/java/com/bluet/bring/getnewsrest/SecurityConfiguration.java
+++ src/main/java/com/bluet/bring/getnewsrest/SecurityConfiguration.java
@@ -1,5 +1,9 @@
 package com.bluet.bring.getnewsrest;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +20,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
@@ -85,7 +90,31 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 		return super.authenticationManagerBean();
 	}
 	
-	
+    // To enable CORS
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+    	
+        
+    	List<String> allowList = new ArrayList<String>();
+
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));  //set access from all domains
+        allowList.clear();
+        allowList = Arrays.asList("GET", "POST", "PUT", "DELETE");    	
+        configuration.setAllowedMethods(allowList);
+        configuration.setAllowCredentials(true);
+        //configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+        allowList.clear();
+        allowList = Arrays.asList("*");    	
+       configuration.setAllowedHeaders(allowList);
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
+    /*
     // To enable CORS
 	@Bean
 	public FilterRegistrationBean<CorsFilter> corsFilter() {
@@ -100,6 +129,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 		bean.setOrder(0);
 		return bean;
 	}
+	*/
 	
 
 	/*
